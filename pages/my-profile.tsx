49afe46a7b7d5f597307e2bf4d9c834c1a90af03
irumvanselme <andesanselme@gmail.com>
2022-08-29 15:05:34 +0200

    feat: make the profile

--- pages/my-profile.tsx
+++ pages/my-profile.tsx
@@ -3,18 +3,21 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Profile from "./[username]";
 
-export default function MyProfile() {
-	const [user, setUser] = useState(null);
-	const router = useRouter();
-	useEffect(() => {
-		(async function () {
-			let user_id = localStorage.getItem("session_id");
-			if (!user_id) return await router.push("/auth/login");
+export default function MyProfile () {
+    const [user, setUser] = useState(null);
 
-			let user = await axios.get("/api/v1/users/" + user_id);
-			setUser(user.data);
-		})();
-	}, []);
-	if (user == null) return <div>Loading...</div>;
-	return <Profile user={user} />;
+    const router = useRouter();
+
+    useEffect(() => {
+        (async function () {
+            let user_id = localStorage.getItem("session_id");
+            if (!user_id) return await router.push("/auth/login");
+
+            let user = await axios.get("/api/v1/users/" + user_id);
+            setUser(user.data);
+        })();
+    }, [router]);
+
+    if (user == null) return <div>Loading...</div>;
+    return <Profile user={user}/>;
 }
